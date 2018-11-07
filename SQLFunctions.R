f979dc1bf6b4f8f6e94c4a801a121638d4cf24fc
Lyon Van Voorhis <lyon.vanvoorhis@gmail.com>
2018-11-07 17:48:02 -0500

    streamlined and pushed a lot of calcs intoo the initial SQL pull

--- SQLFunctions.R
+++ SQLFunctions.R
@@ -8,7 +8,10 @@ pass_source <- " from event where event_type='Pass'"
 pass_fields <- ", pass_recipient_id, pass_recipient_name, pass_length, pass_angle_radians, pass_height, pass_end_xlocation, pass_end_ylocation, pass_type, pass_body_part, pass_outcome, pass_backhe, under_pressure, pass_cross, pass_switch, pass_aerial_won, pass_deflected, pass_miscommunication, pass_assisted_shot_id, pass_goal_assist, pass_shot_assist, pass_through_ball, pass_cut_back"
 match_fields <- ", match_date, kick_off, [competition], season_name, home_team, away_team, home_score, away_score, stadium_name, referee_name, matchup, result, is_a_draw, winner, loser, margin_of_victory"
 
-pass_query <- "select id, matchid, [index], period, [timestamp], minute, second, event_type, possession, possession_team_name, play_pattern_name, off_camera, team_name, duration, player_id, player_name, position_name, ylocation, xlocation, pass_recipient_id, pass_recipient_name, pass_length, pass_angle_radians, pass_height, pass_end_xlocation, pass_end_ylocation, pass_type, pass_body_part, pass_outcome, pass_backhe, under_pressure, pass_cross, pass_switch, pass_aerial_won, pass_deflected, pass_miscommunication, pass_assisted_shot_id, pass_goal_assist, pass_shot_assist, pass_through_ball, pass_cut_back, match_date, kick_off, [competition], season_name, home_team, away_team, home_score, away_score, stadium_name, referee_name, matchup, result, is_a_draw, winner, loser, margin_of_victory from event where event_type='Pass'"
+pass_query <- "select id, matchid, [index], period, [timestamp], minute, second, event_type, possession, possession_team_name, play_pattern_name, off_camera, team_name, duration, player_id, player_name, position_name, e.ylocation, e.xlocation, x.allAroundAveragexG as 'location_xG', pass_recipient_id, pass_recipient_name, pass_length, pass_angle_radians, pass_height, pass_end_xlocation, pass_end_ylocation, y.allAroundAveragexG as 'pass_end_location_xG', y.allAroundAveragexG - x.allAroundAveragexG as 'pass_xG_gain', pass_type, pass_body_part, pass_outcome, pass_backhe, under_pressure, pass_cross, pass_switch, pass_aerial_won, pass_deflected, pass_miscommunication, pass_assisted_shot_id, pass_goal_assist, pass_shot_assist, pass_through_ball, pass_cut_back, match_date, kick_off, [competition], season_name, home_team, away_team, home_score, away_score, stadium_name, referee_name, matchup, result, is_a_draw, winner, loser, margin_of_victory  from event e 
+left join pitchCoordinateValues x on xlocation = x.xlocation_master and ylocation = x.ylocation_master
+left join pitchCoordinateValues y on pass_end_xlocation = y.xlocation_master and pass_end_ylocation = y.ylocation_master
+where e.event_type = 'Pass'"
 
 event_SQL_query <- function(query){
   db <- dbConnect(SQLite(), dbname="~/sqlite/statsBombData.db")
@@ -18,7 +21,7 @@ event_SQL_query <- function(query){
 }
 
 pass_SQL_query <- function(){
-  event_SQL_query(pass_query)
+  result <- event_SQL_query(pass_query)
 }
 
 
@@ -37,12 +40,6 @@ players_SQL_query <- function(){
   result
 }
 
-pitch_values_SQL_query <- function(){
-  db <- dbConnect(SQLite(), dbname="~/sqlite/statsBombData.db")
-  result <- dbGetQuery(db, "select xlocation_master as 'xlocation', ylocation_master as 'ylocation', allAroundAveragexG as 'locationxG' from pitchCoordinateValues")
-  dbDisconnect(db)
-  result
-}
 
 
 
--- dataPrepFunctions.R
+++ dataPrepFunctions.R
@@ -3,26 +3,18 @@ library(dplyr)
 
 
 
-
-attach_pitch_values <- function(event_data){
-  result <- left_join(event_data, pitch_values_SQL_query(), by = c("xlocation" = "xlocation", "ylocation" = "ylocation"))
-}
-
-attach_pass_destination_pitch_values <- function(pass_event_data){
-  data <- pitch_values_SQL_query()
-  rename(data, pass_destinationxG = locationxG) #colnames(data)[colnames(data)=="locationxG"] <- "passDestinationxG"
-  result <- left_join(pass_event_data, data, by =  c("pass_end_xlocation" = "xlocation", "pass_end_ylocation" = "ylocation") )
-}
-
-
-get_pass_value_events <- function(){
-  result <- attach_pitch_values(pass_SQL_query()) %>% attach_pass_destination_pitch_values()
-}
-
 team_pass_events <- function(team_name_input){
-  result <- filter(get_pass_value_events(), team_name == team_name_input)
+  result <- filter(pass_SQL_query(), team_name == team_name_input)
 }
 
 player_pass_events <- function(player_name_input){
-  result <- filter(get_pass_value_events(), player_name == player_name_input)
+  result <- filter(pass_SQL_query(), player_name == player_name_input)
 }
+
+rank_top_xG_gain_passers <- function(pass_data_input){
+  result <- pass_data_input %>% group_by(player_name, team_name) %>% 
+    summarize(count = n(), total_pass_xG_gain = sum(pass_xG_gain, na.rm = TRUE)) %>%
+    arrange(desc(total_pass_xG_gain))
+}
+
+head(rank_top_xG_gain_passers(filter(pass_SQL_query(), pass_outcome == "Complete")))
